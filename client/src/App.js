d6a641faecd89718bec07063cbc3192607115184
Eden Gilligan <edengilligan@Edens-MBP.modem>
2021-04-15 17:51:02 +1000

    user setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import TimeContainer from "./components/TimeContainer";
+import TimeFormEdit from "./components/TimeFormEdit";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
-import TimeForm from "./components/TimeForm";
 import './Style.css'
 
 export const App = () => {
@@ -17,3 +17,24 @@ export const App = () => {
     </Router>
   );
 };
+
+
+<Router>
+<div className="App">
+
+<Link to="/timesheet">Home</Link>
+        <Switch>
+          <Route path="/timesheet">
+          <TimeContainer />
+          </Route>
+        </Switch>
+
+        <Link to="/timesheet/edit">Add</Link>
+        <Switch>
+          <Route path="/timesheet">
+          <TimeFormEdit />
+          </Route>
+        </Switch>
+
+</div>
+</Router>
--- timesheet-server/config/routes.rb
+++ timesheet-server/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get '/welcome', to: "welcome#index"
   namespace :api do
     resources :timesheets
-    resources :users
+    
   end
 end
