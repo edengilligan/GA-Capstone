7bdc14be4fe1c15c4bd468513b2f00275ff7ff2d
Eden Gilligan <edengilligan@Edens-MBP.modem>
2021-04-20 13:02:10 +1000

    user back end working

--- timesheet-server/app/models/user.rb
+++ timesheet-server/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :todos
+    has_many :timesheets
   
     validates :name, uniqueness: { message: 'User already exists' }
   end
--- timesheet-server/config/routes.rb
+++ timesheet-server/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get '/welcome', to: "welcome#index"
   namespace :api do
+    post 'auth/login', to: 'auth#login'
     resources :timesheets
-    
+    resources :users 
   end
 end
