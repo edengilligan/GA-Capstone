28a78f555ef705dccd57b21d98b41914729e38e8
Eden Gilligan <edengilligan@Edens-MBP.modem>
2021-04-25 15:55:41 +1000

    login page css started

--- client/src/components/User/UserForm.js
+++ client/src/components/User/UserForm.js
@@ -28,28 +28,37 @@ export const UserForm = () => {
        window.localStorage.setItem('token', data.token) 
        if (data.token) {
         history.replace('/home')
-      }
-    })
-  }
+      }})}
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={submitHandler}>
-        <label>
-          Username:
-          <input name="name" value={form.name} onChange={changeHandler} />
-        </label>
-        <label>
-          Password:
-          <input
-            name="password"
-            value={form.password}
-            onChange={changeHandler}
-          />
-        </label>
-        <button type="submit">Login</button>
-        <button type="submit">Register</button>
-      </form>
+    <form onSubmit={submitHandler}>
+      <style>
+@import url('https://fonts.googleapis.com/css2?family=Alegreya&display=swap');
+</style>
+      <div class="container px-4 py-5 mx-auto">
+    <div class="card card0">
+        <div class="d-flex flex-lg-row flex-column-reverse">
+            <div class="card card1">
+                <div class="row justify-content-center my-auto">
+                    <div class="col-md-8 col-10 my-5">
+                    <img src="https://media.giphy.com/media/UWk4MNsTLWKoBkV0tt/source.gif"width="150" height="150" frameBorder="0" allowFullScreen></img>
+                        <h3 class="mb-5 text-center heading">Staff Portal</h3>
+                        {/* <h6 class="msg-info">Please login to your staff profile</h6> */}
+                        <div class="form-group"> <label class="form-control-label text-muted">Username</label> <input name="name" type="text" id="email" value={form.name} placeholder="Phone no or email id" class="form-control" onChange={changeHandler} ></input></div>
+                        <div class="form-group"> <label class="form-control-label text-muted">Password</label> <input name="password" type="password" id="psw" value={form.password} placeholder="Password" class="form-control" onChange={changeHandler} ></input></div>
+                        <div class="row justify-content-center my-3 px-3"> <button type="submit" class="btn-block btn-color">Login</button> </div>  
+                    </div>
+                </div>
+                <div class="bottom text-center mb-5">
+                   <div> <p href="#" class="sm-text mx-auto mb-3">Don't have an account? </p></div><p><button type="submit" class="btn btn-white ml-2">Create new</button></p>
+                </div>
+            </div>
+            <div class="card card2">
+                <div class="my-auto mx-md-5 px-md-5 right">
+                </div>
+            </div>
+            </div>
+        </div>
     </div>
-  );
-};
+    </form>
+  )
+}
